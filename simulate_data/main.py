f9ee903c80cb2bc38aa74cd6ceaf985578e40ab5
huazhixixi <huazhi.lun@sjtu.edu.cn>
2020-07-17 11:24:06 +0800

    version 1

--- simulate_data/main.py
+++ simulate_data/main.py
@@ -28,19 +28,19 @@ def simulate_one_span(signal, linear_fiber, nonlinear_fiber, edfa, wss):
     power_after = np.sum(np.mean(np.abs(signal.samples) ** 2, axis=-1))
     gain = np.sqrt(power_after / power_now)
     signal[:] = gain * signal[:]
-
+    import cupy as cp
     noise_sequence = copy.deepcopy(edfa.noise_sequence)
-
+    noise_sequence = cp.asnumpy(noise_sequence)
     # linear prop
     second_signal = linear_fiber.prop(second_signal)
-    second_signal = np.sqrt( 10 ** ((linear_fiber.alpha * linear_fiber.length) / 10)) * second_signal[:]
+    second_signal[:] = np.sqrt( 10 ** ((linear_fiber.alpha * linear_fiber.length) / 10)) * second_signal[:]
     second_signal[:] += noise_sequence
     power_now = np.sum(np.mean(np.abs(second_signal.samples) ** 2, axis=-1))
     second_signal = wss.prop(second_signal)
     power_after = np.sum(np.mean(np.abs(second_signal.samples) ** 2, axis=-1))
     gain = np.sqrt(power_after / power_now)
     second_signal[:] = gain * second_signal[:]
-
+    signal.cpu()
     return signal, signal[:] - second_signal[:]
 
 
@@ -53,15 +53,15 @@ def genreate_signal(signal_power):
 
 
 
-def simulate(config,config_ith):
+def simulate(config,config_ith,power):
 
-    power = config[0]
+    #power = config[0]
     nonlinear_fibers = []
     linear_fibers = []
     nonlinear_noises = []
     signal = genreate_signal(power)
     tx_samples = copy.deepcopy(signal.samples)
-    for kind in range(config[1:]):
+    for kind in config[1:]:
         alpha = FIBER[kind]['alpha']
         D = FIBER[kind]['D']
         gamma = FIBER[kind]['gamma']
@@ -70,14 +70,14 @@ def simulate(config,config_ith):
 
         linear_fibers.append(LinearFiber(alpha,D,80,0,1550))
     
-    for linear_fiber,nonlinear_fiber in zip((linear_fibers,nonlinear_fibers)):
+    for linear_fiber,nonlinear_fiber in zip(linear_fibers,nonlinear_fibers):
         edfa = ConstantGainEdfa(gain=nonlinear_fiber.length * nonlinear_fiber.alpha, nf=5, is_ase=True)
         wss = WSS(0,37.5e9,8.8e9)
         signal, nonlinear_noise_one_span = simulate_one_span(signal, linear_fiber, nonlinear_fiber, edfa, wss)
         
         nonlinear_noises.append(nonlinear_noise_one_span)
 
-    signal.save(f'{power}_{config_ith}', is_mat_file=True)
+    signal.save(f'{base_dir}/{power}_{config_ith}', is_mat_file=True)
     
     from scipy.io import loadmat,savemat
     data = loadmat(f'{base_dir}/{power}_{config_ith}.mat')
@@ -86,12 +86,15 @@ def simulate(config,config_ith):
 
     savemat(f'{base_dir}/{power}_{config_ith}.mat', data)
     
-
+import tqdm
     
 def main(config_name):
     import joblib
     total_config = joblib.load(config_name)
     # 此处可以开多进程
-    for config_ith in range(100):
-        config = total_config[config_ith]
-        simulate(config,config_ith)
+    for power in tqdm.tqdm(range(4)):
+        config = total_config[0]
+        simulate(config,0,power)
+
+
+main('config_setting')
