aa9b769d1f7800ee107de3f7e3d9b600aeeb6030
lun <huazhi.lun@sjtu.edu.cn>
2020-07-20 06:33:44 +0800

    calc_x_db

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
-from library import NonlinearFiber,LinearFiber
+from library import NonlinearFiber, LinearFiber
+import numpy as np
 FIBER = {
     1: {"alpha": 0.2, 'D': 16.7, 'gamma': 1.3},
     2: {"alpha": 0.18, "D": 20.1, "gamma": 0.9},
@@ -15,3 +16,26 @@ def fiber_for_reconstruct(config):
         fibers.append(LinearFiber(alpha,D,80,0,1550))
 
     return fibers
+
+
+def calc_xdb_bandwidth(freq,spectrum,x):
+    from scipy.signal import findfreqs
+    from scipy.interpolate import interp1d
+    spectrum = spectrum / np.max(spectrum)
+    spectrum = 10 * np.log10(spectrum)
+
+    argmax = np.argmax(spectrum)
+    freq_1 = freq[:argmax]
+    freq_2 = freq[argmax:]
+
+    function1 = interp1d(spectrum[:argmax], freq_1)
+    freq1 = function1(-x)
+
+    function2 = interp1d(spectrum[:argmax], freq_1)
+    freq2 = function2(x)
+    return freq2 - freq1
+
+    
+
+    
+    
