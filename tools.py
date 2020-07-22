4946514c26a6093319cacfd66c25f92e6b0edd04
lun <huazhi.lun@sjtu.edu.cn>
2020-07-22 20:42:33 +0800

    push

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
 from library import NonlinearFiber, LinearFiber
 import numpy as np
+import visdom
 FIBER = {
     1: {"alpha": 0.2, 'D': 16.7, 'gamma': 1.3},
     2: {"alpha": 0.18, "D": 20.1, "gamma": 0.9},
@@ -21,21 +22,40 @@ def fiber_for_reconstruct(config):
 def calc_xdb_bandwidth(freq,spectrum,x):
     from scipy.signal import findfreqs
     from scipy.interpolate import interp1d
+    
     spectrum = spectrum / np.max(spectrum)
     spectrum = 10 * np.log10(spectrum)
-
-    argmax = np.argmax(spectrum)
-    freq_1 = freq[:argmax]
-    freq_2 = freq[argmax:]
+    argmax = int(len(spectrum)/2)
+    freq_1 = freq[:int(len(spectrum)/2)]
+    freq_2 = freq[int(len(spectrum)/2):]
 
     function1 = interp1d(spectrum[:argmax], freq_1)
     freq1 = function1(-x)
 
-    function2 = interp1d(spectrum[:argmax], freq_1)
-    freq2 = function2(x)
-    return freq2 - freq1
+    function2 = interp1d(spectrum[argmax:], freq_2)
+    freq2 = function2(-x)
+    return np.abs(freq2 - freq1)/1e9,freq1/1e9,freq2/1e9
 
+
+        
+
+
+
+def main():
+    from scipy.io import loadmat
+    x = loadmat('./data/0_1.mat')['freqs'][0]
+    spec = loadmat('./data/0_1.mat')['spec_lms'][0,:1024]
+    bandwidth_3db,freq1_3,freq2_3 = calc_xdb_bandwidth(x, spec, 3)
+    bandwidth_6db,freq1_6,freq2_6 = calc_xdb_bandwidth(x,spec,6)
+    xielv1 = 3 / (freq1_3 - freq1_6)
+    xielv2 = 3 / (freq2_3 - freq2_6)
+
+
+
+
+    print('hello world')
     
+main()
 
     
     
